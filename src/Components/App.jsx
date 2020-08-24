028c06c1c715bc900e33571d4eca98b4a5151a82
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-24 14:38:55 +0530

    Bug fix

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,6 +8,11 @@ import youtube from "../API/youtubeAPI";
 
 class App extends React.Component {
   state = { videos: [] , selectedVideo:null};
+
+  componentDidMount(){
+    this.onTermSubmit('');
+  }
+
   onTermSubmit = async (term) => {
     //var response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${term}&key=${YT_API}`);
     var response = await youtube.get("/search", {
@@ -16,7 +21,7 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({ videos: response.data.items,selectedVideo: response.data.items[0] });
   };
 
   onVideoSelect=(video)=>{
@@ -31,10 +36,10 @@ class App extends React.Component {
         {/*  I have {this.state.videos.length} videos  */}
         <div className="ui grid">
           <div className="ui row">
-            <div className="eleven wide column">
+            <div className="ten wide column">
               <VideoDetail video={this.state.selectedVideo}/>
             </div>
-            <div className="five wide column">
+            <div className="six wide column">
               <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
             </div>
           </div>

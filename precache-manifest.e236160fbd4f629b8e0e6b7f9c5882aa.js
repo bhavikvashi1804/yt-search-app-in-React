18a94b33f82038e3738e5541ac20e5ec7823db59
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-24 14:44:05 +0530

    Updates

---
+++ precache-manifest.e236160fbd4f629b8e0e6b7f9c5882aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0840c0ebae326de705831c358491823",
+    "url": "/yt-search-app-in-React/index.html"
+  },
+  {
+    "revision": "51819346438c3c4e06d0",
+    "url": "/yt-search-app-in-React/static/css/main.269dd2c7.chunk.css"
+  },
+  {
+    "revision": "ee1a42cae4fd17cd103a",
+    "url": "/yt-search-app-in-React/static/js/2.7e923fd3.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/yt-search-app-in-React/static/js/2.7e923fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51819346438c3c4e06d0",
+    "url": "/yt-search-app-in-React/static/js/main.6d4d7d4d.chunk.js"
+  },
+  {
+    "revision": "4585edd0c10993f7e053",
+    "url": "/yt-search-app-in-React/static/js/runtime-main.4d0f1e11.js"
+  }
+]);

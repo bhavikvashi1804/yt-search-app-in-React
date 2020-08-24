35fda84e8c5d2e25fec28a2dd6e00c6fb0f0a040
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-24 12:11:59 +0530

    store the loaded data from youtube api into state

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,18 +4,21 @@ import SearchBar from "./SearchBar";
 import youtube from "../API/youtubeAPI";
 
 class App extends React.Component {
+  state = { videos: [] };
   onTermSubmit = async (term) => {
-    var response=await youtube.get("/search", {
+    var response = await youtube.get("/search", {
       q: term,
     });
 
-    console.log(response);
+    //console.log(response);
+    this.setState({videos:response.data.items})
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos
       </div>
     );
   }

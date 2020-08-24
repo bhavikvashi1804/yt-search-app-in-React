cfc173d68ec308a4c47ec8de2b9793e870ce28f5
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-24 13:00:16 +0530

    Display the thumbnails on screen

--- src/Components/VideoItem.jsx
+++ src/Components/VideoItem.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const VideoList=({video})=>{
     return (
         <div>
+            <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
             {video.snippet.title}
         </div>
     );
